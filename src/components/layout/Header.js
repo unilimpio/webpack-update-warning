42bbfd4dda9cd73d423e565d6aed6ffb0e7ff3b1
Marcel Filbig <mfilbig@unilimpio.com>
2022-02-01 12:23:49 -0500

    mensaje solidaridad aluvion quito correccion de padding

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -11,10 +11,10 @@ export default function Header({title, desc, banner, pathname, article, articleD
         <Seo title={title} desc={desc} banner={banner} pathname={pathname} article={article} articleDate={articleDate} product={product} category={category} categorySlug={categorySlug} productSku={productSku} productGtin13={productGtin13} offerPrice={offerPrice} brandName={brandName} brandLogo={brandLogo}/>
         <section id="wilmer">
           <div className="w-full mx-auto bg-black">
-            <div className="m-2 text-white">
+            <div className="px-8 py-2 text-white">
               <span className="text-2xl">Aluvión en Quito</span><br/>
               <span className="text-lg">En este momento de mucho dolor, nos solidarizamos con las familias de las víctimas y damnificados que ha dejado el terrible aluvión del pasado Lunes 31 de enero en Quito. </span><br/>
-              <span className="text-md">Estaremos recibiendo donaciones de víveres y frazadas en nuestras oficina Matriz, mismas que serán movilizadas a los albergues para los damnificados.</span>
+              <span className="text-md">Estaremos recibiendo donaciones de víveres y frazadas en nuestras oficina Matriz, mismas que serán movilizadas a los albergues dispuestos para los damnificados.</span>
             </div>
           </div>
         </section>
